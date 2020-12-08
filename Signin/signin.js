76d70a244774c113486f3b3f63360efb527405a5
Olagesin Samuel <feyisamuell@gmail.com>
2020-12-08 15:03:38 +0100

    signin with firebase

---
+++ Signin/signin.js
@@ -0,0 +1,23 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAP4scYff5IptGqdso7x2g2qEVHWVwH_mE",
+    authDomain: "safepay-f4d6f.firebaseapp.com",
+    projectId: "safepay-f4d6f",
+    databaseURL: "https://safepay-f4d6f-default-rtdb.firebaseio.com/",
+    storageBucket: "safepay-f4d6f.appspot.com",
+    messagingSenderId: "666232623305",
+    appId: "1:666232623305:web:d5df501e773fe999693d18",
+    measurementId: "G-7B4255Y8FE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  document.getElementById("regForm").addEventListener("submit", signInForm);
+
+
+  function signInForm(event) {
+      event.preventDefault();
+
+      let email = document.getElementById("email-signin").value;
+      let password = document.getElementById("password-signin").value;
+  }
