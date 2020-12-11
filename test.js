7633882908c8415d01158443418f72a975e16877
Olagesin Samuel <feyisamuell@gmail.com>
2020-12-11 14:17:20 +0100

    node backend

--- test.js
+++ test.js
@@ -21,19 +21,34 @@ var firebaseConfig = {
         document.getElementById("intro").innerHTML = "Nothing here";
     }
   });
+// function SendMail(){
+//   var API_KEY = 'f9e7bade1b61259ceab29083e24a6047-4879ff27-4ee83a82';
+// var DOMAIN = 'sandbox00757d358ebf4c6799d124728c7cf740.mailgun.org';
+// var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+
+// const data = {
+//   from: 'SafePay <feyisamuell@gmail.com>',
+//   to: 'lukii5212@gmail.com',
+//   subject: 'Hello',
+//   text: 'Testing some Mailgun awesomeness!'
+// };
+
+// mailgun.messages().send(data, (error, body) => {
+//   console.log(body);
+// });
+// }
+
 function SendMail(){
-  var API_KEY = 'YOUR_API_KEY';
-var DOMAIN = 'YOUR_DOMAIN_NAME';
-var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
-
+const mailgun = require("mailgun-js");
+const DOMAIN = 'sandbox00757d358ebf4c6799d124728c7cf740.mailgun.org';
+const mg = mailgun({apiKey: "f9e7bade1b61259ceab29083e24a6047-4879ff27-4ee83a82", domain: DOMAIN});
 const data = {
-  from: 'SafePay <feyisamuell@gmail.com>',
-  to: 'lukii5212@gmail.com',
-  subject: 'Hello',
-  text: 'Testing some Mailgun awesomeness!'
+	from: 'Excited User <feyisauell@gmail.com>',
+	to: 'lukii5212@gmail.com, Olagesin@sandbox00757d358ebf4c6799d124728c7cf740.mailgun.org',
+	subject: 'Hello',
+	text: 'Testing some Mailgun awesomness!'
 };
-
-mailgun.messages().send(data, (error, body) => {
-  console.log(body);
+mg.messages().send(data, function (error, body) {
+	console.log(body);
 });
 }
